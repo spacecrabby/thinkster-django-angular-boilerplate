b4381b902b11723a5921bf29176f53017d8443b1
belskikh <belskikh.aleksandr@gmail.com>
2016-01-26 16:34:53 +0300

    added url

--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url, include
 from rest_framework_nested import routers
 
 from thinkster_django_angular_boilerplate.views import IndexView
-from authentication.views import AccountViewSet
+from authentication.views import AccountViewSet, LoginView
 
 router = routers.SimpleRouter()
 router.register(r'accounts', AccountViewSet)
@@ -11,4 +11,5 @@ urlpatterns = patterns(
     '',
     url(r'api/v1/', include(router.urls)),
     url('^.*$', IndexView.as_view(), name='index'),
+    url(r'^api/v1/auth/login/$', LoginView.as_view(), name='login'),
 )
