fd7a66e482b3e6c22f53f680d5c74cbc329a83ab
belskikh <belskikh.aleksandr@gmail.com>
2016-01-26 16:39:29 +0300

    added login functions

--- static/javascripts/authentication/services/authentication.service.js
+++ static/javascripts/authentication/services/authentication.service.js
@@ -21,6 +21,7 @@
     * @desc The Factory to be returned
     */
     var Authentication = {
+      login: login,
       register: register
     };
  
@@ -44,5 +45,19 @@
         email: email
       });
     }
+
+    /**
+    * @name login
+    * @desc Try to log in with email `email` and password `password`
+    * @param {string} email The email entered by the user
+    * @param {string} password The password entered by the user
+    * @returns {Promise}
+    * @memberOf thinkster.authentication.services.Authentication
+    */
+    function login(email, password) {
+      return $http.post('/api/v1/auth/login/', {
+        email: email, password: password
+      });
+}
   }
 })();
